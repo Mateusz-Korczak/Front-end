aef899fc3c12d439b8bf35435098638c73571b41
Mateusz Korczak <korczak.programowanie@gmail.com>
2020-09-19 13:27:05 +0200

    first commit

---
+++ CourseJavaScript/Theory_JS/10-Tablice-Wprowadzenie/test.js
@@ -0,0 +1,10 @@
+console.log("Working mode");
+
+function addNumbers(x, y) {
+    return x + y;
+}
+
+
+
+let resultNum = addNumbers();
+console.log(resultNum);
---
+++ CourseJavaScript/Theory_JS/13-Funkcje_cz1/test.js
@@ -0,0 +1,7 @@
+console.log("Working mode");
+
+const showUserName = function (userName) {
+    console.log("Witaj " + userName);
+}
+
+showUserName("Tomek");
